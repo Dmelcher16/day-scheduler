4efd67eefa10dcf2d3db785e64a9b4259e16fa71
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-08-13 16:03:10 -0700

    added in the timer

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # day-scheduler
-<!-- Day planner that changes with the time of day -->
+<!-- Day planner that changes with the time of day to show if events are past, present, or future. -->
--- schedule.js
+++ schedule.js
@@ -1,8 +1,36 @@
-setInterval(function(){
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-}, 1000)
+setInterval(function () {
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+}, 1000);
 
-$(".saveBtn").on("click", function(){
-    const val = $(this).siblings("textarea")[0].value
-    console.log(val)
-  })
+let schedule = localStorage.getItem("schedule");
+if (schedule) {
+  schedule = JSON.parse(schedule);
+} else {
+  schedule = {};
+}
+
+const currentHour = parseInt(moment().format("H"));
+$(".time-block").each(function () {
+  const hour = parseInt($(this).attr("id").replace("hour-", ""));
+  if (hour < currentHour) {
+    $(this).addClass("past");
+  } else if (currentHour === hour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
+
+for (id in schedule) {
+  $("#" + id)
+    .children("textarea")
+    .val(schedule[id]);
+}
+
+$(".saveBtn").on("click", function () {
+  const val = $(this).siblings("textarea")[0].value.trim();
+  const id = $(this).parent().attr("id");
+  schedule[id] = val;
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+});
+
